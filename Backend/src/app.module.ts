188a45830cd9087d1acb6780504725854b81967b
AbdulhadiDeveloper <dev.alhadi@gmail.com>
2026-07-07 15:13:35 +0500

    feat: setup aws serverless structure

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -27,7 +27,7 @@ import { PaymentsModule } from './payments/payments.module';
         return {
           type: 'postgres',
           ...(url
-            ? { url, ssl: process.env.NODE_ENV === 'production' && !url.includes('postgres:5432') ? { rejectUnauthorized: false } : false }
+            ? { url, ssl: { rejectUnauthorized: false }, extra: { ssl: { rejectUnauthorized: false } } }
             : {
                 host: config.get<string>('DB_HOST'),
                 port: config.get<number>('DB_PORT'),
